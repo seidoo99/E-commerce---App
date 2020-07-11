91a9ec26520ac88da598d2ca52da7d90fb16f61a
seid zerihun <seidoo99@gmial.com>
2020-07-11 10:01:15 -0400

    push changes

--- react-backend/server.js
+++ react-backend/server.js
@@ -1,5 +1,5 @@
 const express = require('express')
-
+const mongoose = require('mongoose')
 
 require('dotenv').config();
 const app = express();
@@ -10,5 +10,11 @@ const port = process.env.PORT || 5000
 
 app.use(express.json());
 
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true});
+const connection = mongoose.connection;
+connection.once('open', ()=> {
+    console.log('mongodb connection established successfully');
+})
 
 app.listen(port, ()=> {console.log(`server start running on port:  ${port}`)})
