90b0f5a8b4122fe01a005c547e683d3a0792e4ac
Abdella <abdella.shukur2@gmail.com>
2020-07-15 21:08:59 -0400

    connected mongoDb database to express backend

--- react-backend/server.js
+++ react-backend/server.js
@@ -1,28 +1,54 @@
-//  const express = require('express')
-const express = require ("express");
-const productdata = require ('./productData')
+const express = require("express");
+// const productdata = require ('./productData')
 const mongoose = require('mongoose')
+const routes = require("./routes")
 
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000
 
 
+
+const Schema = new mongoose.Schema({
+    title: String,
+    content: String
+    },
+    {
+        collection: 'posts'
+    });
+  
+  const Post = mongoose.model('Post', Schema);
+
 app.use(express.json());
-app.get('/api/products', (req, res) => {
-    res.send(productdata.products);
-})
 
-app.get("/api/products/:id", (req, res) => {
-    const productId = req.params.id;
-    const product = productdata.products.find(p=>p.id === productId);
-    // if(product){
-    //     res.send(product)
-    // }
-    // res.status(404).send({message :'product not found'});
-    res.send(productdata.products.find(x=>x.id == productId ))
+app.get('/posts', async (req, res) => {
+      const posts = await Post.find();
+  res.send(posts)
+});
+  
+app.post("/posts", async (req, res) => {
+    const post = new Post({
+      title: req.body.title,
+      content: req.body.content
+    })
+    await post.save()
+    res.send(post)
+  })
+  
+// app.get('/api/products', (req, res) => {
+//     res.send(productdata.products);
+// })
 
-})
+// app.get("/api/products/:id", (req, res) => {
+//     const productId = req.params.id;
+//     const product = productdata.products.find(p=>p.id === productId);
+//     // if(product){
+//     //     res.send(product)
+//     // }
+//     // res.status(404).send({message :'product not found'});
+//     res.send(productdata.products.find(x=>x.id == productId ))
+
+// })
 
 
 
@@ -31,6 +57,7 @@ mongoose.connect('mongodb+srv://tlXx04pN5TdI:tlXx04pN5TdI@cluster0.ulvxj.mongodb
 const connection = mongoose.connection;
 connection.once('open', ()=> {
     console.log('mongodb connection established successfully');
+    
 })
 
 app.listen(port, ()=> {console.log(`server start running on port:  ${port}`)})
