2ec862c48480f1761cdcdd27d9e3cba27517c1ec
yohannes <yohannesbog@gmail.com>
2020-07-16 20:23:27 -0400

    add cart

--- react-backend/server.js
+++ react-backend/server.js
@@ -1,35 +1,48 @@
-//  const express = require('express')
-const express = require ("express");
-const productdata = require ('./productData')
+const express = require("express");
 const mongoose = require('mongoose')
 
-require('dotenv').config();
+require('dotenv').config({ path: 'C:/Users/abdel/OneDrive/Desktop/E-commerce-App/.env' });
 const app = express();
 const port = process.env.PORT || 5000
 
 
+const Schema = new mongoose.Schema({
+    picture: String,
+    brand: String,
+    type: String,
+    price: Number,
+    qut: Number,
+    description: String,
+    count: Number
+    },
+    {
+        collection: 'products'
+    });
+  
+  const Products = mongoose.model('Products', Schema);
+
 app.use(express.json());
-app.get('/api/products', (req, res) => {
-    res.send(productdata.products);
-})
 
-app.get("/api/products/:id", (req, res) => {
-    const productId = req.params.id;
-    // const product = productdata.products.find(p=>p.id === productId);
-    // if(product){
-    //     res.send(product)
-    // }
-    // res.status(404).send({message :'product not found'});
-     res.send(productdata.products.find(x=>x.id == productId ))
+app.get('/api/products', async (req, res) => {
+      const productData = await Products.find();
+  res.send(productData)
+});
+
+
+app.get("/api/products/:id", async (req, res) => {
+  const productData = await Products.find( { _id: '5f0e3c967f6b4d09c7e27555'} )
+  res.send(productData)
 
 })
+
 
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true});
+mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
 const connection = mongoose.connection;
 connection.once('open', ()=> {
     console.log('mongodb connection established successfully');
+    
 })
 
 app.listen(port, ()=> {console.log(`server start running on port:  ${port}`)})
