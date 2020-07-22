3185e8631bb53c5c94efbb5ca4ee135c9d1eb7cb
seid zerihun <seidoo99@gmial.com>
2020-07-22 06:45:40 -0400

    node module update

--- react-backend/config.js
+++ react-backend/config.js
@@ -4,6 +4,6 @@ dotenv.config();
 
 module.exports = {
   PORT: process.env.PORT || 5000,
-  MONGODB_URL: process.env.MONGODB_URL || "mongodb+srv://seidoo:0910287818@mongopractice-mlu2z.mongodb.net/e-commerce?retryWrites=true&w=majority",
+  MONGODB_URL: process.env.MONGODB_URL || "mongodb+srv://tlXx04pN5TdI:tlXx04pN5TdI@cluster0.ulvxj.mongodb.net/ecommerce?retryWrites=true&w=majority",
   JWT_SECRET: process.env.JWT_SECRET || "somethingsecret",
 };
