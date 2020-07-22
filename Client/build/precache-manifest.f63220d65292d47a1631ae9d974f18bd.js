5b2b5cd63455918ec8862b7e49e49694a552f85d
seid zerihun <seidoo99@gmial.com>
2020-07-22 14:19:06 -0400

    deployed to heroku

---
+++ Client/build/precache-manifest.f63220d65292d47a1631ae9d974f18bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "493a60edf55c6df78d5188f222031131",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bfa84f9376efe391f01b",
+    "url": "/static/css/main.1caddd5c.chunk.css"
+  },
+  {
+    "revision": "f99c76ba3ba0251b4421",
+    "url": "/static/js/2.b7ed6d06.chunk.js"
+  },
+  {
+    "revision": "6b3248a1692933689dcb27b9e1ed5092",
+    "url": "/static/js/2.b7ed6d06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfa84f9376efe391f01b",
+    "url": "/static/js/main.f1116c69.chunk.js"
+  },
+  {
+    "revision": "92461eaf6c72e9c4d70e",
+    "url": "/static/js/runtime-main.45f34afd.js"
+  },
+  {
+    "revision": "113945106775b0c5930d06cee62dd0f8",
+    "url": "/static/media/logo.11394510.svg"
+  }
+]);
